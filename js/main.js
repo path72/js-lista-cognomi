5cc55a517a5a438e7ca21883568d77497b3a31e1
Salvatore Trimarchi <path72@gmail.com>
2021-02-24 16:07:13 +0100

    cose varie

--- js/main.js
+++ js/main.js
@@ -3,23 +3,23 @@
 
 // global variables
 var usrSurnameValue, 
-    // checkOK = 0,
+    formErase = false,
+    listErase = false,
     surnameList = [
       'Bianchi',
       'Rossi',
       'Duzioni',
       'Balsano',
       'Verdi',
-      '',
-      '',
-      '',
-      '',
-      '',
-      ''
+      'Giallini',
+      'Santamaria',
+      'Favino',
+      'Mastandrea',
+      'Accorsi'
     ];
 
-// email list in console
-console.log('-- surname list -----------------------');
+// starting list in console
+console.log('-- original surname list -----------------------');
 for (var i = 0; i < surnameList.length; i++) {
   console.log('#'+(i+1)+' '+surnameList[i]);
 }
@@ -28,38 +28,99 @@ for (var i = 0; i < surnameList.length; i++) {
 var usrSurnameForm = document.getElementById('usr_surname');
 
 // info display hooks
-var msgHtml       = document.getElementById('msg');
-var checkMsgHtml  = document.getElementById('check_msg');
+var msgHtml           = document.getElementById('msg');
+var checkMsgHtml      = document.getElementById('check_msg');
+var displayResultHtml = document.getElementById('display_result');
+var resultListHtml    = document.getElementById('result_list');
 
-// ** SURNAME INSERTION **
-var checkBtn = document.getElementById('check_btn');
+// button hooks
+var checkBtn     = document.getElementById('check_btn');
+var eraseBtn     = document.getElementById('erase_btn');
+var resumeBtn    = document.getElementById('resume_btn');
+var insertionBtn = document.getElementById('insertion_btn');
+
+// ** SURNAME CHECK **
 checkBtn.addEventListener('click', 
   function() {
-
-    // data retrieving
+    // form data retrieving
     usrSurnameValue = usrSurnameForm.value;
     console.log('usrSurnameValue = ' + usrSurnameValue);
-
     // consistency check
     if (usrSurnameValue == '') {
       msgHtml.className = 'show';
       checkMsgHtml.innerHTML = 'Compila il campo cognome';
     } else {
-
-      // se non è già presente
-      // aggiungi usrSurnameValue a surnameList
-
-      // se è già presente
-      // non fare nessuna aggiunta
-
-      // riordina la lista (forse duplicando)
-      // surnameList.sort();
-
-      // mostra la lista ordinata
-
-      // indica la posizione di usrSurnameValue
-
+      // surnname check
+      var index = null;
+      for (var i=0; i<surnameList.length; i++) {
+        if (usrSurnameValue == surnameList[i]) index = i;
+      }
+      if (index != null) {
+        // surname already in list: show notice
+        checkMsgHtml.innerHTML = usrSurnameValue+' è già presente in lista';
+        console.log(usrSurnameValue+' già presente');
+        formErase = true;
+      } else {
+        // surname not in list: show notice and insertion button
+        checkMsgHtml.innerHTML = usrSurnameValue+' non è presente in lista';
+        console.log(usrSurnameValue+' non presente');
+        insertionBtn.className = 'show';
+        formErase = false;
+      }
+      msgHtml.className = 'show';
     }
+  }
+);
+
+// ** CHECK REDO **
+eraseBtn.addEventListener('click', 
+  function() {
+    usrSurnameForm.value = '';
+  }
+);
+
+// ** CHECK RESUME **
+resumeBtn.addEventListener('click', 
+  function() {
+    msgHtml.className = 'hide';
+    checkMsgHtml.innerHTML = '';
+    insertionBtn.className = 'hide';
+    if (formErase) usrSurnameForm.value = '';
+    if (listErase) {
+      displayResultHtml.className = 'hide';
+      resultListHtml.innerHTML = '';
+    }
+  }
+);
+
+// ** SURNAME INSERTION **
+insertionBtn.addEventListener('click', 
+  function() {
+    // adding surname to list
+    surnameList.push(usrSurnameValue);
+    console.log(usrSurnameValue+' aggiunto')
+    // capitalize list
+    for (var i = 0; i < surnameList.length; i++) {
+      surnameList[i] = surnameList[i][0].toUpperCase() + surnameList[i].substring(1).toLowerCase();
+    }
+    // sorting capitalized list
+    surnameList.sort();
+    // surname position & final list content
+    console.log('-- new surname list -------------------');
+    var index = null;
+    usrSurnameValue = usrSurnameValue[0].toUpperCase() + usrSurnameValue.substring(1)
+    for (var i = 0; i < surnameList.length; i++) {
+      console.log('#'+(i+1)+' '+surnameList[i]);
+      if (usrSurnameValue == surnameList[i]) index = i;
+      resultListHtml.innerHTML += (index == i) ? '<li><span class="strong">#'+(i+1)+' '+surnameList[i]+'</span></li>' : '<li>#'+(i+1)+' '+surnameList[i]+'</li>';
+    }
+    // insertion message
+    insertionBtn.className = 'hide';
+    checkMsgHtml.innerHTML = usrSurnameValue+' è stato aggiunto in posizione <span class="strong">#'+(index+1)+'</span>';
+    formErase = true;
+    listErase = true;
+    // show list
+    displayResultHtml.className = 'show';
   }
 );
 
